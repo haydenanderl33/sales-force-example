069dc1568fb306bf30ec736772433208f8509836
haydenanderl33 <hayden.anderl33@gmail.com>
2021-04-20 15:07:51 -0600

    good for mobile

--- src/App.js
+++ src/App.js
@@ -8,35 +8,32 @@ class App extends Component {
 
     this.state = {
       search: null,
-      name:"",
-      age:"",
-      country:""
+      name: "",
+      age: "",
+      country: "",
     };
   }
 
   searchSpace = (event) => {
     let keyword = event.target.value;
     this.setState({ search: keyword });
-    console.log(this.state.search)
+    console.log(this.state.search);
   };
 
   handleChange(event) {
-    this.setState({value: event.target.name});
+    this.setState({ value: event.target.name });
   }
 
   fillFields = (data_id) => {
-    let item = Information.find(e => e.id === data_id)
+    let item = Information.find((e) => e.id === data_id);
 
-    this.setState({name: item.name});
-    this.setState({age: item.age});
-    this.setState({country: item.country});
-    console.log(item.name)
-    console.log(item.age)
-    console.log(item.country)
-
-
-  
-  }
+    this.setState({ name: item.name });
+    this.setState({ age: item.age });
+    this.setState({ country: item.country });
+    console.log(item.name);
+    console.log(item.age);
+    console.log(item.country);
+  };
 
   render() {
     const items = Information.filter((data) => {
@@ -45,43 +42,55 @@ class App extends Component {
         data.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
         data.country.toLowerCase().includes(this.state.search.toLowerCase())
       ) {
-        
         return data;
       }
       return "";
     }).map((data) => {
       return (
-        <div key={data.id} >
-            <ul>
-              <li className="items">
-                <span> {data.name}  </span>
-                <span> {data.age}   </span>
-                <span> {data.country}   </span>
-                <button onClick={()=>this.fillFields(data.id)}>Fill Fields</button>
-              </li>
-            </ul>
-        </div>
+        
+          <ul key={data.id}>
+            <li className="items">
+              <span> {data.name} </span>
+              <span> {data.age} </span>
+              <span> {data.country} </span>
+              <button onClick={() => this.fillFields(data.id)}>
+                Fill Fields
+              </button>
+            </li>
+          </ul>
       );
     });
 
     return (
-      <>
-      <div className="inputCont">
-        <input
-          type="text"
-          placeholder=" Enter name to be searched"
-          onChange={(e) => this.searchSpace(e)}
-        />
+      <div className="app">
+        <div className="searchCont">
+          <input
+            type="text"
+            placeholder=" Enter name to be searched"
+            onChange={(e) => this.searchSpace(e)}
+          />
         </div>
         <div className="dataContainer">
-           <div className="searchItemsContainer">{items}</div>
-           <div className="inputContainer">
-             <input value={this.state.name} onChange={this.handleChange} placeholder=" Name"/>
-             <input value={this.state.age} onChange={this.handleChange}  placeholder=" Age"/>
-             <input value={this.state.country} onChange={this.handleChange} placeholder=" Location"/>
-           </div>
-           </div>
-      </>
+          <div className="searchItemsContainer">{items}</div>
+          <div className="inputContainer">
+            <input
+              value={this.state.name}
+              onChange={this.handleChange}
+              placeholder=" Name"
+            />
+            <input
+              value={this.state.age}
+              onChange={this.handleChange}
+              placeholder=" Age"
+            />
+            <input
+              value={this.state.country}
+              onChange={this.handleChange}
+              placeholder=" Location"
+            />
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/Information.js
+++ src/Information.js
@@ -5,13 +5,14 @@ const Information = [
   { id: 2, name: "Sam", age: 22, country: "USA" },
   { id: 3, name: "Mark", age: 23, country: "Africa" },
   { id: 4, name: "Markus", age: 34, country: "Africa" },
-  { id: 5, name: "Aayush", age: 45, country: "India" },
+  { id: 5, name: "Rajesh", age: 45, country: "India" },
   { id: 6, name: "Sean", age: 18, country: "Ireland" },
   { id: 7, name: "Eduardo", age: 24, country: "France" },
   { id: 8, name: "Dustin", age: 26, country: "Spain" },
-  { id: 9, name: "Alexendra", age: 27, country: "USA" },
+  { id: 9, name: "Alexandra", age: 27, country: "USA" },
   { id: 10, name: "Lee", age: 21, country: "China" },
   { id: 11, name: "Jim", age: 21, country: "Korea" },
+  { id: 12, name: "Steve", age: 25, country: "Mozambique" },
 ];
 
 export default Information;
